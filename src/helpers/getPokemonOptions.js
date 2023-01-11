6d38c43082d945baf1cef28c054d8795fc8e2d48
OliverosA <aldair_oliveros09@hotmail.es>
2023-01-11 13:46:43 -0600

    axios added and getPokemonsOptions completed

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,16 +1,45 @@
+import pokemonApi from "@/api/pokemonApi";
+
 const getPokemons = () => {
   const pokemonsArr = Array.from(Array(650));
 
   return pokemonsArr.map((value, index) => index + 1);
 };
 
-const getPokemonsOptions = () => {
+const getPokemonsOptions = async () => {
   const mixedPokemonts = getPokemons().sort(() => Math.random() - 0.5);
-  getPokemonsNames(mixedPokemonts.splice(0, 4));
+  const pokemons = await getPokemonsNames(mixedPokemonts.splice(0, 4));
+  return pokemons;
 };
 
-const getPokemonsNames = ([a, b, c, d] = []) => {
-  console.log(a, b, c, d);
+const getPokemonsNames = async ([a, b, c, d] = []) => {
+  const promiseArr = [
+    //multiples promesas
+    pokemonApi.get(`/${a}`),
+    pokemonApi.get(`/${b}`),
+    pokemonApi.get(`/${c}`),
+    pokemonApi.get(`/${d}`),
+  ];
+  const [p1, p2, p3, p4] = await Promise.all(promiseArr);
+
+  return [
+    {
+      name: p1.data.name,
+      id: p1.data.id,
+    },
+    {
+      name: p2.data.name,
+      id: p2.data.id,
+    },
+    {
+      name: p3.data.name,
+      id: p3.data.id,
+    },
+    {
+      name: p4.data.name,
+      id: p4.data.id,
+    },
+  ];
 };
 
 export default getPokemonsOptions;
