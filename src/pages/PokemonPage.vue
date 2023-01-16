e67c459df51396e636ff2f1c165ebbc9e0c1b83a
OliverosA <aldair_oliveros09@hotmail.es>
2023-01-16 10:21:53 -0600

    base game completed

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -2,8 +2,14 @@
   <h1 v-if="!pokemon">Espere por favor...</h1>
   <div v-else>
     <h1>¿Quién es este pokemon?</h1>
+
     <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
     <PokemonOptions :pokemons="pokemonArr" @selection="checkAnswer" />
+
+    <template v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo Juego</button>
+    </template>
   </div>
 </template>
 
@@ -23,6 +29,8 @@ export default {
       pokemonArr: [],
       pokemon: null,
       showPokemon: false,
+      showAnswer: false,
+      message: "",
     };
   },
   methods: {
@@ -31,8 +39,22 @@ export default {
       const rndInt = Math.floor(Math.random() * 4);
       this.pokemon = this.pokemonArr[rndInt];
     },
-    checkAnswer(pokemonId) {
+    checkAnswer(selectedId) {
       this.showPokemon = true;
+      this.showAnswer = true;
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `Correcto, ${this.pokemon.name}`;
+      } else {
+        this.message = `Oops, era ${this.pokemon.name}`;
+      }
+    },
+    newGame() {
+      this.pokemonArr = [];
+      this.pokemon = null;
+      this.showPokemon = false;
+      this.showAnswer = false;
+      this.mixPokemonArray();
     },
   },
   mounted() {
